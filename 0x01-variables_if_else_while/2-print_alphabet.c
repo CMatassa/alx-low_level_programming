c53673feb5a1a07c7ff6a0f6c82234f87e5ab9db
CMatassa <cmatassa28@icloud.com>
2023-03-20 19:48:02 -0700

    Updated Variables

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Prints Alphabet
+ * main - main function
  *
- * Return: 0.
+ * Return: 0
  */
 int main(void)
 {
+	char i;
+
 	for (i = 'a' ; i <= 'z' ; i++)
 		putchar(i);
 	putchar('\n');
