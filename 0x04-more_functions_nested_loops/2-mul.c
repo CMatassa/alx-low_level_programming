3fe98b8780c1cd6289cd8023809ab613c8d6e48e
CMatassa <cmatassa28@icloud.com>
2023-03-23 15:20:35 -0700

    Updated more functions

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies 2 integers
+ * @a: fit int
+ * @b: second int
+ * Return: addition of multiplication
+ */
+int mul(int a, int b)
+
+{
+	return (a * b);
+}
